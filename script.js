303d2d70c439cbe6cce7f50f945b8ce284a8fc28
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 15:00:23 -0400

    Add 16x16 grid of squares
    Add basic grid with little functionality.

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("rowContainer");
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("click", (e) => {
+            console.log("clicked");
+        });
+        row.appendChild(square);
+    }
+    container.appendChild(row);
+}
