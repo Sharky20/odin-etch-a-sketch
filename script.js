ece91133017b9a8809ebe7ab0cab3e9fc01d49ed
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 15:13:17 -0400

    Make grid resizable
    Can change the number of squares on the grid by changing size.
    Fits mostly into the same area as before changing size.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector(".container");
+let size = 14;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.classList.add("rowContainer");
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
         let square = document.createElement("div");
         square.classList.add("square");
+        square.style.padding = `${20*16 / size}px`;
         square.addEventListener("mouseover", (e) => {
             square.classList.add("colored");
         });
